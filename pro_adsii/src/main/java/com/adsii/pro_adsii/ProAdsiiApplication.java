e709cbab4052e242ffceb6293d3560accf1e3b2b
AlejandroYaquian <jyaquians@miumg.edu.gt>
2025-08-06 16:40:37 -0600

    Proyecto inicial

---
+++ pro_adsii/src/main/java/com/adsii/pro_adsii/ProAdsiiApplication.java
@@ -0,0 +1,13 @@
+package com.adsii.pro_adsii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProAdsiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProAdsiiApplication.class, args);
+	}
+
+}
---
+++ pro_adsii/src/test/java/com/adsii/pro_adsii/ProAdsiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adsii.pro_adsii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProAdsiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
