1fa83fbf655b84c7e819f270ebf381817acb1430
DonaldProgramacion2020 <68457519+DonaldProgramacion2020@users.noreply.github.com>
2025-08-28 23:08:56 -0600

    se agrega service de tabla opcion

---
+++ pro_adsii/src/main/java/com/adsii/pro_adsii/Service/OpcionService.java
@@ -0,0 +1,42 @@
+package com.adsii.pro_adsii.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adsii.pro_adsii.entity.Opcion;
+import com.adsii.pro_adsii.repository.OpcionRepository;
+
+@RestController
+@RequestMapping("/opcion")
+@Service
+
+public class OpcionService {
+
+	@Autowired
+	OpcionRepository ur;
+	
+	@GetMapping(path="/buscar")
+	
+	public List<Opcion> buscar(){
+		return ur.findAll();
+	}
+	
+	@PostMapping(path="/guardar")
+	public Opcion guardar(@RequestBody Opcion opcion) {
+		return ur.save(opcion);
+	}
+	
+	@DeleteMapping(path="/eliminar/{idopcion}")
+	public void eliminar(@PathVariable int idopcion) {
+		ur.deleteById(idopcion);
+	}
+}
