22ea57d99185b7d44bc77c39a1dc52ba90a9ccd5
DonaldProgramacion2020 <68457519+DonaldProgramacion2020@users.noreply.github.com>
2025-08-29 22:30:24 -0600

    actualizacion service tabla menu

--- pro_adsii/src/main/java/com/adsii/pro_adsii/Service/MenuService.java
+++ pro_adsii/src/main/java/com/adsii/pro_adsii/Service/MenuService.java
@@ -1,5 +1,44 @@
-package com.adsii.pro_adsii.Service;
+package com.adsii.pro_adsii.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adsii.pro_adsii.entity.Menu;
+import com.adsii.pro_adsii.repository.MenuRepository;
+
+
+@RestController
+@RequestMapping("/menu")
+@Service
 
 public class MenuService {
 	
+	@Autowired
+	MenuRepository ur;
+	
+	@GetMapping(path ="/buscar")
+	
+	public List<Menu> buscar(){
+		return ur.findAll();
+	}
+	
+	@PostMapping(path="/guardar")
+	public Menu guardar(@RequestBody Menu menu) {
+		return ur.save(menu);
+	}
+	
+	@DeleteMapping(path="/eliminar/{idmenu}")
+	public void eliminar(@PathVariable int idmenu) {
+		ur.deleteById(idmenu);
+	}
+
 }
