7a68f6dce961b37573898511e34d13f94d7d01b5
AlejandroYaquian <jyaquians@miumg.edu.gt>
2025-09-02 14:28:33 -0600

    Pantalla de inicio y otras correcciones

--- pro_adsii/src/main/java/com/adsii/pro_adsii/Controller/GeneroController.java
+++ pro_adsii/src/main/java/com/adsii/pro_adsii/Controller/GeneroController.java
@@ -2,49 +2,39 @@ package com.adsii.pro_adsii.Controller;
 
 import com.adsii.pro_adsii.Entity.Genero;
 import com.adsii.pro_adsii.Service.GeneroService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+
+@RestController
 @RequestMapping("/genero")
 public class GeneroController {
 
-    @Autowired
-    private GeneroService generoService;
+    private final GeneroService generoService;
+
+    public GeneroController(GeneroService generoService) {
+        this.generoService = generoService;
+    }
 
     @GetMapping("/listar")
-public String listarGeneros(Model model) {
-    model.addAttribute("generos", generoService.listarTodos());
-    model.addAttribute("genero", new Genero()); // Para el formulario
-    return "genero";
-}
+    public List<Genero> listar() {
+        return generoService.listarTodos();
+    }
 
-
-    @GetMapping("/nuevo")
-    public String mostrarFormularioNuevo(Model model) {
-        model.addAttribute("genero", new Genero());
-        return "genero/formulario";
+    @GetMapping("/{id}")
+    public Genero obtener(@PathVariable Long id) {
+        return generoService.obtenerPorId(id).orElse(null);
     }
 
     @PostMapping("/guardar")
-    public String guardarGenero(@ModelAttribute Genero genero) {
-        generoService.guardar(genero);
-        return "redirect:/genero/listar";
+    public Genero guardar(@RequestBody Genero genero) {
+        return generoService.guardar(genero);
     }
 
-    @GetMapping("/editar/{id}")
-public String mostrarFormularioEditar(@PathVariable Long id, Model model) {
-    model.addAttribute("generos", generoService.listarTodos());
-    model.addAttribute("genero", generoService.obtenerPorId(id).orElse(new Genero()));
-    return "genero";
-}
-
-
-    @GetMapping("/eliminar/{id}")
-    public String eliminarGenero(@PathVariable Long id) {
+    @DeleteMapping("/eliminar/{id}")
+    public void eliminar(@PathVariable Long id) {
         generoService.eliminar(id);
-        return "redirect:/genero/listar";
     }
 }
